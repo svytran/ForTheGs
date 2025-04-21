564f8ad64d3b7ff38ec162aa5e9dcca30b129dd1
Shana Tran <vytran@ad.unc.edu>
2025-04-21 00:10:10 -0400

    Initial Commit

---
+++ ForTheGs/ForTheGsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForTheGsApp.swift
+//  ForTheGs
+//
+//  Created by Shana Tran on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ForTheGsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
